9f581c04e7f20ad6180b02c16edea171963f2227
sabrina <kiamilev@usc.edu>
2017-09-11 16:51:00 -0700

    routing automatically routes to login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import {Home, Login} from './pages';
 import {CreateProfile} from './pages';
@@ -8,11 +8,22 @@ import {CreateProfile} from './pages';
 const App = () => (
     <Router>
         <div>
-            <Route exact path="/" component={Home} />
             <Route path="/login/" component={Login} />
-        	<Route path ="/createprofile" component={CreateProfile}/>
+        	<PrivateRoute authed={false} path ="/createprofile" component={CreateProfile}/>
+            <PrivateRoute exact authed={false} path="/" component={Home} />
         </div>
     </Router>
 );
+
+function PrivateRoute ({component: Component, authed, ...rest}) {
+    return (
+        <Route
+            {...rest}
+            render={(props) => authed === true
+                ? <Component {...props} />
+                : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+        />
+    )
+}
 
 export default App;
