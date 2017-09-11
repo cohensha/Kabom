c5a5460644cf2bfed30a8d9d1a96d708a4d380ae
Sharon Cohen <cohensha@usc.edu>
2017-09-10 22:00:58 -0700

    starting to add to profile creation form

--- src/pages/createprofile/FormContainer.js
+++ src/pages/createprofile/FormContainer.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Button, form, FormGroup, Label, Input, FormText, ButtonGroup} from 'reactstrap'; 
+import { Button, form, FormGroup, Label, Input, FormText, ButtonGroup, DropdownItem, DropdownToggle, ButtonDropdown, DropdownMenu} from 'reactstrap'; 
 
 /**********************LINKING TO FIREBASE FOR TESTING***/
 
@@ -21,6 +21,7 @@ var firebase = require("firebase");
   var database = firebase.database();
 
 
+
   //*****************************************************
 
 class FormContainer extends Component {
@@ -30,6 +31,21 @@ class FormContainer extends Component {
 		super(props);
 
 		this.handleFormSubmit = this.handleFormSubmit.bind(this);
+
+		this.toggleU = this.toggleU.bind(this);
+		this.state = {
+			dropdownOpen: false
+		};
+
+		this.onCheckboxBtnClick = this.onCheckboxBtnClick.bind(this);
+		this.state = { cSelected: [] };
+
+	}
+
+	toggleU() {
+		this.setState({
+			dropdownOpen: !this.dropdownOpen
+		});
 	}
 
 	mountComponent() {
@@ -39,7 +55,8 @@ class FormContainer extends Component {
 			.then(data => {
 
 				this.setState({
-					
+					roles: data.roles,
+					universities: data.universities
 					
 				});
 
@@ -55,14 +72,72 @@ class FormContainer extends Component {
 	}
 
 
+	  onCheckboxBtnClick(selected) {
+		    const index = this.state.cSelected.indexOf(selected);
+		    if (index < 0) {
+		      this.state.cSelected.push(selected);
+		    } else {
+		      this.state.cSelected.splice(index, 1);
+		    }
+		    this.setState({ cSelected: [...this.state.cSelected] });
+		  }
+
 	render() {
 
 		return (
-				<ButtonGroup>
-			        <Button>Left</Button>{' '}
-			        <Button>Middle</Button>{' '}
-			        <Button>Right</Button>	
-		    	</ButtonGroup>
+				// <ButtonGroup>
+			 //        <Button>Left</Button>{' '}
+			 //        <Button>Middle</Button>{' '}
+			 //        <Button>Right</Button>	
+		  //   	</ButtonGroup>
+
+		  <form className = "container" onSubmit={this.handleFormSubmit()}>
+
+
+	  			<ButtonGroup className="role-buttons">
+				        <Button>Developer</Button>{' '}
+				        <Button>Product manager</Button>{' '}
+				        <Button>Designer</Button>	
+	    		</ButtonGroup>
+
+	    		<br/>
+
+	    		<ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+				    <DropdownToggle caret>
+				          Button Dropdown
+				    </DropdownToggle>
+				    <DropdownMenu>
+			          <DropdownItem header></DropdownItem>
+			          <DropdownItem >University of Southern California</DropdownItem>
+			          <DropdownItem>UCLA</DropdownItem>
+			          <DropdownItem>Stanford University</DropdownItem>
+				     </DropdownMenu>
+     			</ButtonDropdown>
+
+
+
+     			<div className="skillsButtons">
+				     	<h5>Skills Buttons</h5>
+				        <ButtonGroup>
+				          <Button color="primary" onClick={() => this.onCheckboxBtnClick(1)} active={this.state.cSelected.includes(1)}>One</Button>
+				          <Button color="primary" onClick={() => this.onCheckboxBtnClick(2)} active={this.state.cSelected.includes(2)}>Two</Button>
+				          <Button color="primary" onClick={() => this.onCheckboxBtnClick(3)} active={this.state.cSelected.includes(3)}>Three</Button>
+				        </ButtonGroup>
+				        <p>Selected: {JSON.stringify(this.state.cSelected)}</p>
+     			</div>
+     		</form>
+		    
+
+
+
+     		// Button.propTypes = {
+     		// 	active: propTypes.bool,
+     		// 	color: propTypes.string,
+
+     		// 	onClick: propTypes.func
+     		// }
+
+
 
 
 		 );
