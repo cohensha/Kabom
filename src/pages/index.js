a92dd844269e440f853348a96eb698bedc11ec8e
Sharon Cohen <cohensha@usc.edu>
2017-10-30 15:38:07 -0700

    404 page to merge

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,13 @@ import Login from './login/login'
 import CreateProfile from './createprofile/createprofile';
 import Header from './header/header';
 import Sidebar from './sidebar/sidebar';
-import ProjectProfile from './projectprofile/projectprofile';
 import NotFound from './notfound/notFound';
 
 export {
     Home,
     Login,
-    CreateProfile,
     Header,
     Sidebar,
-    ProjectProfile,
+    CreateProfile,
     NotFound
 }
